b1c6a5ce61976bfc80a2d4ac7785d64fbeee0f08
Shashank09 <shashanktulsikar979@gmail.com>
2023-12-11 19:54:20 +0530

    tenth commit

--- js/main.js
+++ js/main.js
@@ -160,7 +160,3 @@ close.addEventListener("click",function(){
   loginPopup.classList.remove("show");
 })
 
-(function(w,d,u){
-    var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
-    var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
-})(window,document,'https://cdn.bitrix24.in/b26320415/crm/site_button/loader_1_3witws.js');
